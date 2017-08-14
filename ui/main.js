8b55f5fc3f1a0ac2abec5e9f44055b6e69fb4f49
shsm1981 <30905801+shsm1981@users.noreply.github.com>
2017-08-14 22:37:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,8 +5,8 @@ var counter=0;
 button.onclick=function(){
 
     counter=counter+1;
-      alert(counter);
+    
     var span=document.getElementById('count');
-   
+     alert(counter);
     span.innerHTML=counter.toString();
 };
