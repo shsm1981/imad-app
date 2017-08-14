49cef4ec5e65cb1d36e50b42251ca9637e5dd379
shsm1981 <30905801+shsm1981@users.noreply.github.com>
2017-08-15 00:30:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,8 +15,8 @@ var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
     var request=new XMLHttpRequest();
     request.onreadystatechange=function(){
-        if(request.readyState==XMLHttpRequest.DONE){
-        if(request.status==200){
+        if(request.readyState===XMLHttpRequest.DONE){
+        if(request.status===200){
             var names=request.responseText;
             names=JSON.parse(names);
   
@@ -31,6 +31,6 @@ submit.onclick=function(){
 };
 var nameInput=document.getElementById('name');
 var name=nameInput.value;
-request.open('GET','http://http://shsm1981.imad.hasura-app.io/sbmit_name?name'+name,true);
+request.open('GET','http://http://shsm1981.imad.hasura-app.io/submit-name?name'+name,true);
 request.send(null);
 };
