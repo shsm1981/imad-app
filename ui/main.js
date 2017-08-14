a96f69cea571ef9a0c01c16853e663f40b7c9c71
shsm1981 <30905801+shsm1981@users.noreply.github.com>
2017-08-14 23:09:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,7 +11,7 @@ button.onclick=function(){
     span.innerHTML=counter.toString();
 };
 var nameInput=document.getElementById('name');
-var name=nameInput.value;
+//var name=nameInput.value;
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
     var names=['name1','name2','name3','name4'];
