15a1513e5c4cbb1b65764bd86bf0a64a26452c16
shsm1981 <30905801+shsm1981@users.noreply.github.com>
2017-08-15 12:50:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,7 +13,12 @@ app.get('/counter',function(req,res){
     counter=counter+1;
     res.send(counter.toString());
 });
-
+var names=[];
+app.get('/submit-name',function(req,res){
+var name=req.query.name;
+names.push(name);
+res.send(JSON.stringify(names));
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
