0acf6566f35bf49c654123de49bb4ac155d29690
shsm1981 <30905801+shsm1981@users.noreply.github.com>
2017-08-17 22:09:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -35,6 +35,6 @@ submit.onclick=function(){
 };
 var nameInput=document.getElementById('name');
 var name=nameInput.value;
-request.open('GET','http://http://shsm1981.imad.hasura-app.io/submit-name?name'+name,true);
+request.open('GET','http://http://shsm1981.imad.hasura-app.io/submit-name?name='+name,true);
 request.send(null);
 };
