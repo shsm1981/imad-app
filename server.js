87fb1067dddbc060796bcd2bf13a334623225ab8
shsm1981 <30905801+shsm1981@users.noreply.github.com>
2017-08-17 22:08:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,7 +18,6 @@ app.get('/counter',function(req,res){
 var names=[];
 app.get('/submit-name',function(req,res){
 var name=req.query.name;
-
 names.push(name);
 res.send(JSON.stringify(names));
 });
