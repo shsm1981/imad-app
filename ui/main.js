e84e0690282a9c146d83e042ee1214e71e48dcdd
shsm1981 <30905801+shsm1981@users.noreply.github.com>
2017-08-18 00:16:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,20 +1,17 @@
-
- var button=document.getElementById('counter');
-
-
+var button=document.getElementById('counter');
 button.onclick=function(){
 var request=new XMLHttpRequest();
     request.onreadystatechange=function(){
         if(request.readyState===XMLHttpRequest.DONE){
+            if(request.status===200){
     var counter=request.responseText;
-    counter=counter+1;
-    
     var span=document.getElementById('count');
-    
     span.innerHTML=counter.toString();
         }
-        
+        }
     };
+request.open('GET','http://shsm1981.imad.hasura-app.io/counter',true);
+request.send(null);
 };
 
 var submit=document.getElementById('submit_btn');
