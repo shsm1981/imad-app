3af157094a6ed09015a539b68d5dd4acca09579b
shsm1981 <30905801+shsm1981@users.noreply.github.com>
2017-08-19 19:13:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -41,6 +41,24 @@ names.push(name);
 res.send(JSON.stringify(names));
 });
 
+app.get('/articles/:articleName',function(req,res){
+ pool.query('select * from article where title='+req.params.articleName, function(err,result){
+      if(err){
+          res.status(500).send(err.toString());
+      }else
+      {
+      if(result.rows.length===0)
+      {res.status(404).send('article not found');}
+      else
+      {
+          var articleData=result.rows[0];
+          res.send(createTemplate(articleData));
+      }
+      }
+        });   
+    
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
