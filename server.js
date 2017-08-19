9e743357bbeb45b02e275790787db37f0c1f4ecc
shsm1981 <30905801+shsm1981@users.noreply.github.com>
2017-08-19 19:52:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.get('/articles/:articleName',function(req,res){
       {res.status(404).send('article not found');}
       else
       {
-          var articleData=result.rows[0];
+          var articleData=result.rows[1];
           res.send(createTemplate(articleData));
       }
       }
