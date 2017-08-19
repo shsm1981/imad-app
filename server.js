dee038b6b11d198a5d04ec3286c01a3220d2b252
shsm1981 <30905801+shsm1981@users.noreply.github.com>
2017-08-19 20:32:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -67,8 +67,7 @@ function createTemplate(data){
     var category=data.category;
 
     var htmlTemplate=' 
-    <html><head><title>${title}</title><link href="/ui/style.css" rel="stylesheet" /></head> <body> <h1>${category}</h1><p>${content}</p>    <h3>${date.toDateString()}</h3>    </body></html>  
-    ';
+    <html><head><title>${title}</title></head> <body> <h1>${category}</h1><p>${content}</p>    <h3>${date.toDateString()}</h3>    </body></html>      ';
     return htmlTemplate;
    
 }
