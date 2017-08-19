2331ba3a853c69e85f2c3d4c5abeeab5cb114e18
shsm1981 <30905801+shsm1981@users.noreply.github.com>
2017-08-19 18:52:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var pool=requie('pg').Pool;
+var pool=require('pg').Pool;
 var config={
     user:'shsm1981',
     database:'shsm1981',
